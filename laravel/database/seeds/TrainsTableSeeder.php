ef38ed9434ebbf471cbf4d9a19fee0404b8a3edd
dvthnight <94679147+dvthnight@users.noreply.github.com>
2022-04-22 17:55:29 +0200

    ciclo for per generare 1000 treni

--- laravel/database/seeds/TrainsTableSeeder.php
+++ laravel/database/seeds/TrainsTableSeeder.php
@@ -13,17 +13,19 @@ class TrainsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $train = new Train();
+        for ($i=0; $i < 1000; $i++) { 
+            $train = new Train();
 
-        $train->azienda = $faker->company();
-        $train->stazione_partenza = $faker->city();
-        $train->stazione_arrivo = $faker->city();
-        $train->orario_partenza = $faker->time();
-        $train->orario_arrivo = $faker->time();
-        $train->numero_carrozze = $faker->numberBetween("1","50");
-        $train->in_orario = $faker->numberBetween("0","1");
-        $train->cancellato = $faker->numberBetween("0","1");
+            $train->azienda = $faker->company();
+            $train->stazione_partenza = $faker->city();
+            $train->stazione_arrivo = $faker->city();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->numero_carrozze = $faker->numberBetween("1","50");
+            $train->in_orario = $faker->numberBetween("0","1");
+            $train->cancellato = $faker->numberBetween("0","1");
 
-        $train->save();
+            $train->save();
+        }
     }
 }
